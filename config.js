2fc8a473cbcc427996ee637cdc2e630c7aa68e23
matrinsic <matinex76@gmail.com>
2024-12-29 09:57:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -44,6 +44,6 @@ module.exports = {
         }),
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || " ",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
-  SUDO: process.env.SUDO || "2349112171078,94703981512", // replace ur number
+  SUDO: process.env.SUDO || "2349032455387,94703981512", // replace ur number
   IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
 };
